aa320eeb9e0ffb731c7a0792da1ef965d65878f8
hyunsokstar <hyunsokstar@gmail.com>
2024-02-21 17:32:23 +0900

    update for shortcut copy3

--- src/components/DataGrid/DataGridForSubShortcuts.tsx
+++ src/components/DataGrid/DataGridForSubShortcuts.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useMemo } from 'react';
 import 'react-data-grid/lib/styles.css';
-import { Box, Button, Input, useToast } from '@chakra-ui/react';
+import { Box, Button, Input, useToast, useClipboard, Flex, Editable, EditablePreview, EditableInput } from '@chakra-ui/react';
 import DataGrid from 'react-data-grid';
 import { ITypeForSubShortCutListRow } from '@/types/typeForShortCut';
 
@@ -15,35 +15,44 @@ const getColumnsForSubShortCutList = (toast: any) => {
             key: 'shortcut',
             name: 'Shortcut',
             renderCell({ row, tabIndex, onRowChange }: any): React.ReactNode {
-                const copyShortcut = () => {
-                    // 복사 버튼을 눌렀을 때 row.shortcut을 클립보드에 복사하는 로직
-                    navigator.clipboard.writeText(row.shortcut)
-                        .then(() => {
-                            toast({
-                                title: 'Shortcut Copied!',
-                                status: 'success',
-                                duration: 2000, // 메시지가 표시되는 시간
-                                isClosable: true, // 닫기 버튼 표시 여부
-                                position: 'top', // 메시지가 표시될 위치
-                            });
-                        })
-                        .catch((error) => {
-                            console.error('Failed to copy shortcut: ', error);
-                            toast({
-                                title: 'Copy Failed',
-                                description: 'Failed to copy shortcut. Please try again.',
-                                status: 'error',
-                                duration: 2000,
-                                isClosable: true,
-                                position: 'top', // 메시지가 표시될 위치
-                            });
-                        });
-                };
+
+
+                const { onCopy, value, setValue, hasCopied } = useClipboard(row.shortcut);
+
+                // navigator.clipboard.writeText(row.shortcut)
+                //     .then(() => {
+                //         toast({
+                //             title: 'Shortcut Copied!',
+                //             status: 'success',
+                //             duration: 2000, // 메시지가 표시되는 시간
+                //             isClosable: true, // 닫기 버튼 표시 여부
+                //             position: 'top', // 메시지가 표시될 위치
+                //         });
+                //     })
+                //     .catch((error) => {
+                //         console.error('Failed to copy shortcut: ', error);
+                //         toast({
+                //             title: 'Copy Failed',
+                //             description: 'Failed to copy shortcut. Please try again.',
+                //             status: 'error',
+                //             duration: 2000,
+                //             isClosable: true,
+                //             position: 'top', // 메시지가 표시될 위치
+                //         });
+                //     });
 
                 return (
                     <Box display="flex" alignItems="center">
-                        <Input value={row.shortcut} />
-                        <Button ml={2} onClick={copyShortcut}>복사</Button>
+                        {/* <Input value={row.shortcut} />
+                        <Button ml={2} onClick={copyShortcut}>복사</Button> */}
+                        <Flex mb={2}>
+                            <Input
+                                value={row.shortcut}
+                                mr={2}
+                            />
+                            <Button onClick={onCopy}>{hasCopied ? "Copied!" : "Copy"}</Button>
+                        </Flex>
+
                     </Box>
                 );
             },
