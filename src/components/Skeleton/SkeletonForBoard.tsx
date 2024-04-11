c2eeee9cc840967ea87a27b47b80dbd3e34d2f89
hyunsokstar <hyunsokstar@gmail.com>
2024-04-11 13:29:50 +0900

    update for skeleton

--- src/components/Skeleton/SkeletonForBoard.tsx
+++ src/components/Skeleton/SkeletonForBoard.tsx
@@ -7,13 +7,16 @@ const SkeletonForBoard = (props: Props) => {
     return (
         <Box>
             {[1, 2, 3, 4, 5].map((item) => (
-                <Flex key={item} align="center" justify="space-between" my={2}>
+                <Flex key={item} align="center" justify="space-between" my={2} gap={2}>
                     <Checkbox isChecked={false} isDisabled />
-                    <Flex direction="column" flex="1" ml={2}>
+                    <Flex direction="column" flex="1" mx={2}>
                         <Skeleton height="20px" mb={1} />
-                        <Skeleton height="16px" />
                     </Flex>
-                    <Skeleton height="30px" width="80px" />
+                    <Skeleton height="20px" width="80px" />
+                    <Skeleton height="20px" width="80px" />
+                    <Skeleton height="20px" width="80px" />
+                    <Skeleton height="20px" width="80px" />
+                    <Skeleton height="20px" width="80px" />
                 </Flex>
             ))}
         </Box>
