065801c919eb87fcdbf22956b7270ce94cc948ee
hyunsokstar <hyunsokstar@gmail.com>
2024-05-19 21:33:01 +0900

    update for dev assignment list

--- src/components/Menus/CategoryMenuForDevAssignment.tsx
+++ src/components/Menus/CategoryMenuForDevAssignment.tsx
@@ -1,37 +1,11 @@
-import React from 'react';
-import { Box, Text } from '@chakra-ui/react';
-import { AssignmentCategory } from '@/types/typeForDevRelay';
+import React from 'react'
 
-interface CategoryMenuForDevAssignmentProps {
-    selectedCategory: AssignmentCategory | null; // 선택된 카테고리 상태 추가
-    onSelectCategory: (category: AssignmentCategory) => void;
+type Props = {}
+
+const CategoryMenuForDevAssignment = (props: Props) => {
+    return (
+        <div>CategoryMenuForDevAssignment</div>
+    )
 }
 
-const CategoryMenuForDevAssignment: React.FC<CategoryMenuForDevAssignmentProps> = ({ selectedCategory, onSelectCategory }) => {
-    const handleCategoryClick = (category: AssignmentCategory) => {
-        onSelectCategory(category);
-    };
-
-    return (
-        <Box bg="teal.500" p={4} display="flex">
-            {Object.values(AssignmentCategory).map((category) => (
-                <Box
-                    key={category}
-                    p={2}
-                    cursor="pointer"
-                    _hover={{ bg: 'teal.700', color: 'white' }}
-                    onClick={() => handleCategoryClick(category)}
-                    bg={selectedCategory === category ? 'teal.700' : ''}
-                    color={selectedCategory === category ? 'white' : ''}
-                    borderRadius="md"
-                    mx={1}
-                    transition="all 0.3s"
-                >
-                    <Text>{category}</Text>
-                </Box>
-            ))}
-        </Box>
-    );
-};
-
-export default CategoryMenuForDevAssignment;
+export default CategoryMenuForDevAssignment
