d31d92ffd98616f957ec901a23a9915e4ad2f7e6
hyunsokstar <hyunsokstar@gmail.com>
2024-06-07 23:33:26 +0900

    update for todo2

--- src/components/Table/TableForToDosForDevBattle.tsx
+++ src/components/Table/TableForToDosForDevBattle.tsx
@@ -47,27 +47,42 @@ const TableForToDosForDevBattle = ({ selectedDevBattleId, devBattleSubject, todo
                 mb={4}
                 w="100%"
             >
+                <Box
+                    display={"flex"}
+                    justifyContent={"flex-start"}
+                    alignItems={"center"}
+                    gap={5} mb={2}
+                >
+                    {/* <Text>Subject: {devBattleSubject}</Text> */}
+                    <ModalButtonForAddTodoForDevBattle devBattleId={selectedDevBattleId} />
+                </Box>
+
                 <Table size="sm">
                     <Thead>
                         <Tr>
                             <Th w="10%">ID</Th>
                             <Th w="60%">Title</Th>
                             <Th w="30%">Due Date</Th>
+                            {/* <Th>Description</Th> */}
+                            {/* <Th>Created At</Th> */}
+                            {/* <Th>Updated At</Th> */}
                         </Tr>
                     </Thead>
                     <Tbody>
                         {todos.map((todo) => (
                             <Tr key={todo.id}>
-                                <Td w="10%">{todo.id}</Td>
-                                <Td w="60%">{todo.title}</Td>
-                                <Td w="30%">{formatDateTime(todo.dueDate)}</Td>
+                                <Td>{todo.id}</Td>
+                                <Td>{todo.title}</Td>
+                                <Td>{formatDateTime(todo.dueDate)}</Td>
+                                {/* <Td>{todo.description}</Td> */}
+                                {/* <Td>{formatDateTime(todo.createdAt)}</Td> */}
+                                {/* <Td>{todo.updatedAt}</Td> */}
                             </Tr>
                         ))}
                     </Tbody>
                 </Table>
             </Box>
         </TableContainer>
-
     )
 }
 
